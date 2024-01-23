de030132297a8b9b21d40b8985060881239cefcd
Phinehas Beresford <78656905+Cracko298@users.noreply.github.com>
2024-01-22 19:19:07 -0500

    Add files via upload

---
+++ AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyProduct("Saturn 9")]
+[assembly: Guid("a11e0000-0756-430b-b20a-13648b6b2266")]
+[assembly: AssemblyTitle("Saturn 9")]
+[assembly: AssemblyVersion("1.0.0.0")]
